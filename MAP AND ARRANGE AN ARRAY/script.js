3d8d4f091244726aa06b49db5c7fc9b7ee1d263a
90arm12 <akeel.mannoor@gmail.com>
2025-01-04 10:48:01 -0800

    winny commit

---
+++ MAP AND ARRANGE AN ARRAY/script.js
@@ -0,0 +1,22 @@
+const arr = [60, 30, 10, 20, 90, 50,80, 70, 40];
+
+document.getElementById("show_array").innerHTML = arr;
+
+function asc() {
+    arr.sort(function (a, b) {
+        return a - b;
+    });
+    document.getElementById("show_array").innerHTML = "The ascending order of the data are " +arr;
+}
+function dsc() {
+    arr.sort(function (a, b) {
+        return b - a;
+    });
+    document.getElementById("show_array").innerHTML = "The descending order of the data are " +arr;
+}
+function square() {
+    const mapped_arr = arr.map(function (e) {
+        return e * e;
+    })
+    document.getElementById("mapped_array").innerHTML = mapped_arr;
+}
